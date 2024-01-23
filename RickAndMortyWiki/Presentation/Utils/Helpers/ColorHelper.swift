fe59ffba3facdcd31669d4a9c0b32d96303d1861
ltAldoRaine <beka.gelash@gmail.com>
2024-01-23 11:09:04 +0400

    - Remove extra colors

--- RickAndMortyWiki/Presentation/Utils/Helpers/ColorHelper.swift
+++ RickAndMortyWiki/Presentation/Utils/Helpers/ColorHelper.swift
@@ -12,15 +12,11 @@ enum ColorHelper: String {
     case backgroundColorOne
     case errorColor
     case helpColorOne
-    case helpColorTwo
     case inputsStrokesButtonsColor
     case successColor
-    case textColorFour
     case textColorOne
     case textColorThree
     case textColorTwo
-    case warningColor
-    case test
 
     var color: UIColor {
         UIColor(named: rawValue.capitalizingFirstLetter()) ?? .clear
